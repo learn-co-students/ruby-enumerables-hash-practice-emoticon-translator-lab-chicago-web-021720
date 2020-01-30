31d2957287606629e63c04b769e8b26af0d97e13
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-30 20:18:12 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,18 @@ require 'pry'
 def load_library(file_path)
   # code goes here
   loaded_file = YAML.load_file(file_path)
-  binding.pry
-  loaded_file
+  meanings = {}
+  loaded_file.each do |meaning|
+    meanings[meaning[1][1]] = meaning[0]
+    #binding.pry
+  end
+
+  emoticons = {}
+  loaded_file.each do |meaning|
+    #binding.pry
+    emoticons[meaning[1][0]] = meaning[1][1]
+  end
+  {:get_meaning => meanings, :get_emoticon => emoticons}
 end
 
 def get_japanese_emoticon
